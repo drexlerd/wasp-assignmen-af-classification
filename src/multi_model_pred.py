6f520bedc864f126a0631322b466526494b6475d
Martin Funkquist <martin.funkquist@gmail.com>
2023-08-11 15:09:30 +0200

    Created script for multi model eval

--- src/multi_model_pred.py
+++ src/multi_model_pred.py
@@ -1,60 +1,65 @@
 import argparse
-import os
 from pathlib import Path
 import numpy as np
 import torch
 
-from ConfigSpace import ConfigurationSpace
-from callback import CustomCallback
-from smac import Scenario
-from smac import HyperparameterOptimizationFacade as HPOFacade
-from multiprocessing import freeze_support
+from tqdm import tqdm
+from sklearn.metrics import roc_auc_score, average_precision_score
 
-from train import train
+from src.dataset import get_dataloaders
+
+from train import eval_loop
 
 
 if __name__ == "__main__":
-  freeze_support()
   arg_parser = argparse.ArgumentParser()
-  arg_parser.add_argument("--model_paths", type=int, default=2)
-  arg_parser.add_argument("--n_trials", type=int, default=4)
-  arg_parser.add_argument("--n_epochs", type=int, default=200)
-  arg_parser.add_argument("--walltime_limit", type=int, default=120)
-  arg_parser.add_argument("--output_folder", type=str)
+  arg_parser.add_argument("--models_folder", type=str, required=True)
   args = arg_parser.parse_args()
 
-  cs = ConfigurationSpace({
-    "lr": [1e-5, 1e-4, 1e-3, 1e-2, 1e-1],
-    "kernel_size": [7, 15, 31, 63],
-    "n_res_blks": [1, 2, 3, 4],
-    "dropout_rate": [0.0, 0.5, 0.8],
-    "batch_size": [16, 32, 64],
-    "out_channels": [8, 16, 32, 64, 128],
-    "factor": [2, 4, 8],
-    "n_epochs": [args.n_epochs]
-  })
-
   # set seed
-  seed = 42
+  seed = 0
   np.random.seed(seed)
   torch.manual_seed(seed)
 
-  scenario = Scenario(
-      configspace=cs,
-      output_directory=Path(args.output_folder),
-      walltime_limit=args.walltime_limit,
-      # trial_walltime_limit=10,  # Limit to 30 seconds per trial
-      n_trials=args.n_trials,  # Evaluated max 500 trials
-      # n_workers=8,  # Use eight workers
-      n_workers=args.n_workers,
-  )
+  # Set device
+  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+  tqdm.write("Use device: {device:}\n".format(device=device))
 
-  callback = CustomCallback()
+  loss_function = torch.nn.BCEWithLogitsLoss()
 
-  smac = HPOFacade(scenario=scenario, target_function=train,
-                  #  callbacks=[callback]
-              )
-  incumbent = smac.optimize()
+  batch_size = 32
+  train_dataloader, valid_dataloader, n_classes, len_dataset = get_dataloaders(seed, batch_size)
 
-  with open(os.path.join(args.output_folder, "incumbent.txt"), "w") as f:
-    f.write(str(incumbent))
+  models = []
+  for model_file in Path(args.models_folder).glob("*.pt"):
+    model = torch.load(model_file)
+    model.to(device)
+    models.append(model)
+
+  all_valid_loss = []
+  all_valid_pred = []
+  all_valid_true = []
+  for model in models:
+    valid_loss, valid_pred, valid_true = eval_loop(1, valid_dataloader, model, loss_function, device)
+    all_valid_loss.append(valid_loss)
+    all_valid_pred.append(valid_pred)
+    all_valid_true.append(valid_true)
+
+  assert all(x == all_valid_true[0] for x in all_valid_true)
+  
+  # Majority vote for the predictions
+  preds = []
+  for i in range(len(all_valid_pred[0])):
+    votes = []
+    for j in range(len(all_valid_pred)):
+      votes.append(all_valid_pred[j][i])
+    preds.append(max(set(votes), key = votes.count))
+
+  valid_auroc = roc_auc_score(all_valid_true[0], preds)
+  tqdm.write("Validation AUROC: {:.2f}".format(valid_auroc))
+
+  valid_ap = average_precision_score(all_valid_true[0], preds)
+  tqdm.write("Validation AP: {:.2f}".format(valid_ap))
+
+  
+  
